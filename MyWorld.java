476d94f8eff14344a333c10338318abcb48c2511
kylinzhaoo <117937015+kylinzhaoo@users.noreply.github.com>
2022-12-01 02:49:33 -0500

    elephant, apple, score

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * the world our hero lives in
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author kylin 
+ * @version november 30
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +18,38 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        //Create the elephant object
+        elephant elephant = new elephant();
+        addObject(elephant, 300, 300);
+        
+        //Create a label
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
     }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+        
+        
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+    }
+    
 }
---
+++ elephant.java
@@ -0,0 +1,45 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * the elephant, our hero
+ * 
+ * @author kylin
+ * @version november 30
+ */
+public class elephant extends Actor
+{
+    /**
+     * Act - do whatever the elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        // Remove apple if elephant eats it
+        eat();
+        
+    }
+    
+    /**
+     * Eat the apple and spawns new apple if an apple is eaten
+     */
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
+    }
+}
